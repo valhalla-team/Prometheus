4d15e31dd5e0e21113b9cd9f8dfe5f2d23648577
Bruno <bmdevelop@outlook.fr>
2024-09-20 21:56:39 +0200

    Infrastructure layer added and GitHub GraphQL Service

---
+++ Infrastructure/Services/GitHub/GitHubGraphQLService.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL;
+using GraphQL.Client.Http;
+
+namespace Infrastructure.Services.GitHub;
+
+public class GitHubGraphQLService(GraphQLHttpClient client)
+{
+    private readonly GraphQLHttpClient _client = client;
+
+    public async Task<TResponse> QueryGitHubAsync<TResponse>(string query, object? variables = null)
+    {
+        var request = new GraphQLRequest
+        {
+            Query = query,
+            Variables = variables
+        };
+        var response = await _client.SendQueryAsync<TResponse>(request);
+
+        return response.Data;
+    }
+}
