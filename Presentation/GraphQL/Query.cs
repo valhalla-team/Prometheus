29a22d70e7833dd86f45fcebff885091d09655cc
Bruno <bmdevelop@outlook.fr>
2024-09-20 20:42:14 +0200

    Added HotChocolate

---
+++ Presentation/GraphQL/Query.cs
@@ -0,0 +1,24 @@
+﻿namespace Presentation.GraphQL;
+
+public class Query
+{
+    public UserViewer GetUser() =>
+        new UserViewer
+        {
+            Login = "bmsant",
+            ProfileOwner = new ProfileOwner
+            {
+                Name = "Bruno Santos"
+            }
+        };
+}
+public class UserViewer
+{
+    public string Login { get; set; }
+    public ProfileOwner ProfileOwner { get; set; }
+}
+
+public class ProfileOwner
+{
+    public string Name { get; set; }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,15 @@
+using Presentation.GraphQL;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType<Query>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGraphQL();
 
 app.Run();
